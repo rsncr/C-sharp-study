2032d96c1770c0b7e5627e3ae5a6fa6952777325
admin <arsendragunkin@gmail.com>
2022-08-02 20:25:56 +0300

    Added Example002 Hello User with name entering

---
+++ Example002_Hello_User/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter your name");
+string username = Console.ReadLine();
+Console.Write("Hello, ");
+Console.Write(username);
