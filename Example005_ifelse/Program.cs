2c2cb787eb9bbde6d96ce9394d97f0b2eca4e294
admin <arsendragunkin@gmail.com>
2022-08-02 21:23:07 +0300

    Added Example005 with if else operator

---
+++ Example005_ifelse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter your name:");
+string username = Console.ReadLine();
+if(username.ToLower() == "aldik")
+{
+    Console.WriteLine("Aldik, ty dalbaeb!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.Write(username);
+}
