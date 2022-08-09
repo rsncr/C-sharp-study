7c9b968a5ffce1ae8f0a1223eac2d3469eb6028b
admin <arsendragunkin@gmail.com>
2022-08-09 04:00:27 +0300

    Corrected task13

--- Practice_Lesson_2/Homework/task13/Program.cs
+++ Practice_Lesson_2/Homework/task13/Program.cs
@@ -1,13 +1,15 @@
 ﻿Console.Write("Введите число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 
-if(a > -100 && a < 100)
+int m = Math.Abs(a); //конвертация в модуль от введеного числа
+
+if(m > -100 && m < 100)
 {
     Console.WriteLine("Третьей цифры нет");
 }
 else
 {
 int digitIndex = 2;
-int digit = a.ToString()[digitIndex] - '0';
+int digit = m.ToString()[digitIndex] - '0';
 Console.WriteLine(digit);
 }
