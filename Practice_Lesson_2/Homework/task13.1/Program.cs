ecf4cec57ab1bada68d3737a3698235eae30f881
admin <arsendragunkin@gmail.com>
2022-08-13 19:33:09 +0300

    added 2nd solution for task13

---
+++ Practice_Lesson_2/Homework/task13.1/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Введите число: ");
+string a = Console.ReadLine();
+
+if(a.Contains("-"))
+{
+    if (a.Length < 4)
+    { 
+        Console.WriteLine("Третьей цифры нет");  
+    }
+else
+    {
+    char b = a[3];
+    Console.WriteLine(b);
+    }
+}
+else
+{
+    if (a.Length < 3)
+    { 
+        Console.WriteLine("Третьей цифры нет");  
+
+    }
+    else
+    {
+        char b = a[2];
+        Console.WriteLine(b);
+    }
+}
